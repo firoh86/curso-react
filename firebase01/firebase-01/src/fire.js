e877023b2a641cd4e77b760c8859506a46430597
firoh86 <firohh.as@gmail.com>
2019-12-02 15:04:02 +0100

    login con firebase form terminado

---
+++ firebase01/firebase-01/src/fire.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyDX9gpkGpPgusZU7HIchXg-nROI7AcepNw",
+  authDomain: "fir-01-a9c0c.firebaseapp.com",
+  databaseURL: "https://fir-01-a9c0c.firebaseio.com",
+  projectId: "fir-01-a9c0c",
+  storageBucket: "fir-01-a9c0c.appspot.com",
+  messagingSenderId: "353487722813",
+  appId: "1:353487722813:web:1b7f2bdf55bca98ecc4d54",
+  measurementId: "G-4LVJRG5BS2"
+};
+const fire = firebase.initializeApp(config);
+export default fire;
---
+++ firebase01/firebase-01/src/login/Login.jsx
@@ -0,0 +1,204 @@
+import React, { useState } from "react";
+import "firebase/auth";
+import { useFirebaseApp } from "reactfire";
+
+const Login = () => {
+  const firebase = useFirebaseApp();
+  // por defecto se muestra login
+  const [registerForm, setRegisterForm] = useState(false);
+  // controlador de log
+  const [isloged, setisloged] = useState(false);
+  let user;
+  const [useridForm, setUserIdForm] = useState({
+    email: "",
+    password: "",
+    nickname: "",
+    userUid: ""
+  });
+  const [userid, setUserId] = useState({
+    email: "",
+    password: "",
+    nickname: "",
+    userUid: ""
+  });
+  // toggle del form de inicio
+  const toggleForm = () => setRegisterForm(!registerForm);
+
+  const register = async e => {
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(useridForm.email, useridForm.password)
+      .then(data => {
+        let uid = firebase.auth().currentUser.uid;
+        setUserId({
+          ...userid,
+          email: useridForm.email,
+          password: useridForm.password,
+          userUid: uid
+        });
+        user = firebase.auth().currentUser;
+        setisloged(true);
+        resetValues();
+      })
+      .catch(err => {
+        // Handle Errors here.
+        let errorCode = err.code;
+        let errorMessage = err.message;
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password.");
+          resetValues();
+        } else {
+          alert(errorMessage);
+        }
+        console.log(err);
+      });
+
+    console.log(user);
+  };
+  const login = async () => {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(useridForm.email, useridForm.password)
+      .then(data => {
+        let uid = firebase.auth().currentUser.uid;
+        setUserId({
+          ...userid,
+          email: useridForm.email,
+          password: useridForm.password,
+          userUid: uid
+        });
+        user = firebase.auth().currentUser;
+        setisloged(true);
+        resetValues();
+      })
+      .catch(err => {
+        // Handle Errors here.
+        let errorCode = err.code;
+        let errorMessage = err.message;
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password.");
+          resetValues();
+        } else {
+          alert(errorMessage);
+        }
+        console.log(err);
+      });
+
+    console.log(user);
+  };
+
+  const logout = async () => {
+    await firebase
+      .auth()
+      .signOut()
+      .then(
+        data =>
+          setUserIdForm({
+            ...useridForm,
+            email: "",
+            password: "",
+            nickname: "",
+            userUid: ""
+          }),
+        resetValues("ID")
+      );
+    user = null;
+    setisloged(false);
+    console.log(user);
+  };
+
+  // resetea los valores del form
+  const resetValues = e => {
+    if (e === "ID") {
+      setUserId({
+        email: "",
+        password: "",
+        nickname: "",
+        userUid: ""
+      });
+      console.log("reset user");
+    } else {
+      setUserIdForm({
+        email: "",
+        password: "",
+        nickname: "",
+        userUid: ""
+      });
+      console.log("reset Form");
+    }
+  };
+
+  return (
+    <div className="login">
+      {!isloged ? (
+        <div className="login__row">
+          <h4 className="login__tittle">
+            {registerForm ? "Crear Cuenta" : "Iniciar Sesión"}
+          </h4>
+
+          <input
+            onChange={e =>
+              setUserIdForm({ ...useridForm, email: e.target.value })
+            }
+            type="email"
+            placeholder="email"
+            id="email"
+            value={useridForm.email}
+          ></input>
+          <input
+            onChange={e =>
+              setUserIdForm({ ...useridForm, password: e.target.value })
+            }
+            type="password"
+            placeholder="contraseña"
+            id="contraseña"
+            value={useridForm.password}
+          ></input>
+          {registerForm && (
+            <input
+              onChange={e =>
+                setUserIdForm({ ...useridForm, nickname: e.target.value })
+              }
+              type="text"
+              placeholder="usuario"
+              id="usuario"
+              value={useridForm.nickname}
+            ></input>
+          )}
+          {registerForm ? (
+            <button className="login__button" onClick={register}>
+              register
+            </button>
+          ) : (
+            <button className="login__button" onClick={login}>
+              login
+            </button>
+          )}
+          <p>
+            {registerForm
+              ? "¿Ya estás registrado? "
+              : "¿Aun no tienes cuenta? "}
+            {/* <button onClick={toggleForm}> */}
+            <button
+              onClick={() => {
+                toggleForm();
+                resetValues();
+              }}
+            >
+              {registerForm ? "Accede ya" : "Create una"}
+            </button>
+          </p>
+        </div>
+      ) : (
+        <div className="login__row">
+          <h4 className="login__tittle">Cerrar Sesión</h4>
+          <button className="login__button" onClick={logout}>
+            logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;

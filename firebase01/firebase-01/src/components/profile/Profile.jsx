6e7124f133846059db7ef6d7ab8c06fe2e081b03
firoh86 <firohh.as@gmail.com>
2019-12-13 19:52:26 +0100

    ajustes de estructura

--- firebase01/firebase-01/src/components/profile/Profile.jsx
+++ firebase01/firebase-01/src/components/profile/Profile.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div>
-      <h3>Perfil</h3>
+    <div className="profile__container">
+      <div className="card-profile">
+        <div className="card-profile__image">
+          <img src="" alt=""></img>
+        </div>
+        <div className="card-profile__header">
+          <div className="card-profile__title">
+            <h3 className="card-profile__nickname">Firoh</h3>
+            <p className="card-profile__description">Breve descripcion</p>
+          </div>
+        </div>
+        <div className="card-profile__body">
+          <div className="card-profile__column">
+            <h2 className="card-profile__count">12</h2>
+            <h4 className="card-profile__subtitle">Likes</h4>
+          </div>
+          <div className="card-profile__column">
+            <h2 className="card-profile__count">14</h2>
+            <h4 className="card-profile__subtitle">Followers</h4>
+          </div>
+          <div className="card-profile__column">
+            <h2 className="card-profile__count">18</h2>
+            <h4 className="card-profile__subtitle">Following</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

0375369ff35edea551eb00009548ed7b7a15838f
firoh86 <firohh.as@gmail.com>
2019-12-15 22:14:16 +0100

    arreglar el async con return

--- firebase01/firebase-01/src/components/profile/Profile.jsx
+++ firebase01/firebase-01/src/components/profile/Profile.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// para state de redux
+import { useSelector } from "react-redux";
+import firestoreActions from "../../hooks/firebase/firestoreActions";
 
 const Profile = () => {
+  const state = useSelector(state => state);
+  // autocompletado de perfil
+  const [, ReadUserProfile] = firestoreActions();
+
+  const [profileData, setProfileData] = useState({
+    nickname: "",
+    description: "",
+    likes: 0,
+    followers: 0,
+    following: 0
+  });
+
+  useEffect(() => {
+    const ask = async () => {
+      const res = await ReadUserProfile(state.userid);
+      console.log(res);
+    };
+
+    ask();
+  }, [ReadUserProfile, state.userid]);
+
   return (
     <div className="profile__container">
       <div className="card-profile">
@@ -9,21 +33,23 @@ const Profile = () => {
         </div>
         <div className="card-profile__header">
           <div className="card-profile__title">
-            <h3 className="card-profile__nickname">Firoh</h3>
-            <p className="card-profile__description">Breve descripcion</p>
+            <h3 className="card-profile__nickname">{profileData.nickname}</h3>
+            <p className="card-profile__description">
+              {profileData.description}
+            </p>
           </div>
         </div>
         <div className="card-profile__body">
           <div className="card-profile__column">
-            <h2 className="card-profile__count">12</h2>
+            <h2 className="card-profile__count">{profileData.likes}</h2>
             <h4 className="card-profile__subtitle">Likes</h4>
           </div>
           <div className="card-profile__column">
-            <h2 className="card-profile__count">14</h2>
+            <h2 className="card-profile__count">{profileData.followers}</h2>
             <h4 className="card-profile__subtitle">Followers</h4>
           </div>
           <div className="card-profile__column">
-            <h2 className="card-profile__count">18</h2>
+            <h2 className="card-profile__count">{profileData.following}</h2>
             <h4 className="card-profile__subtitle">Following</h4>
           </div>
         </div>
@@ -33,3 +59,17 @@ const Profile = () => {
 };
 
 export default Profile;
+
+/* 
+
+ return () => {
+      setProfileData({
+        ...profileData,
+        nickname: readData.nickname,
+        description: readData.description,
+        likes: readData.likes,
+        followers: readData.followers,
+        following: readData.following
+      });
+    };
+*/
--- firebase01/firebase-01/src/hooks/firebase/firestoreActions.jsx
+++ firebase01/firebase-01/src/hooks/firebase/firestoreActions.jsx
@@ -2,11 +2,11 @@ import { firestore } from "firebase";
 
 const firestoreActions = () => {
   const SetNewUser = (uid, nickname) => {
-    console.log(`${uid} -- ${nickname}`);
+    // console.log(`${uid} -- ${nickname}`);
     const newData = {
       uid: uid,
       nickname: nickname,
-      description: "",
+      description: "Escribe aqui una breve descripción de ti",
       followers: 0,
       following: 0,
       likes: 0
@@ -17,7 +17,22 @@ const firestoreActions = () => {
       .doc(newData.uid)
       .set(newData);
   };
-  return [SetNewUser];
+
+  const ReadUserProfile = uid => {
+    firestore()
+      .collection("users")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          const data = doc.data();
+          if (uid === data.uid) {
+            // guardar la funcion de llamada en una constante para extraer datos
+            return data;
+          }
+        });
+      });
+  };
+  return [SetNewUser, ReadUserProfile];
 };
 
 export default firestoreActions;
